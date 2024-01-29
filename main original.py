79ad39b06adcf70046048b28d7a995a28cba3bf8
aglamazlarefe <aglamazlarefe@gmail.com>
2024-01-29 18:47:35 +0300

    arayuz güncellendi ses tanıma sistemi çalışmıyor

--- main original.py
+++ main original.py
@@ -1,8 +1,10 @@
+import string
 from textwrap import fill
 import tkinter as tk
 from tkinter import BOTTOM, ttk
 import os
 from pathlib import Path
+from matplotlib.pylab import f
 from numpy import Infinity
 import cv2
 from PIL import Image, ImageTk
@@ -13,16 +15,9 @@ import vosk
 import json
 
 
-
-
 #pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 current_directory = os.getcwd() 
-
-
-
-
-
 
 
 class MainApplication(tk.Tk):
@@ -37,7 +32,7 @@ class MainApplication(tk.Tk):
 
         self.frames = {}
 
-        for F in (anasayfa, optik, speech_reco, yazı, el_tanıma_1,el_tanıma_2):
+        for F in (anasayfa, optik, speech_reco,  el_tanıma_1,el_tanıma_2):
             frame = F(container, self) 
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -56,113 +51,39 @@ class anasayfa(tk.Frame):
 
         self.controller= controller
         # Keep references to tk.PhotoImage objects
+
+        #frame ***4****        
+
+        def relative_to_assets(file: str):
+            file= "Screens/assets/frame4/"+ file
+            return file
         
+        
+
         canvas = tk.Canvas(self,bg="#47C4B6",height=320,width=480,bd=0,highlightthickness=0,relief="ridge",)
 
         canvas.place(x = 0, y = 0)
         
-        self.button_image_1 = tk.PhotoImage(file="Screens/assets/frame5/button_1.png")
-        button_1 = tk.Button(self, image=self.button_image_1,borderwidth=0,highlightthickness=0,command=lambda: controller.show_frame(yazı),relief="flat")
-        button_1.place(x=15.0, y=10.0, width=450.0, height=85.0)
+        self.button_image_1 = tk.PhotoImage(file=relative_to_assets("button_1.png"))
+        button_1 = tk.Button(self,image=self.button_image_1,borderwidth=0,highlightthickness=0,command=lambda: controller.show_frame(optik),relief="flat")        
+        button_1.place(x=15.0,y=10.0,width=450.0,height=85.0)
 
 
-        self.button_image_2 = tk.PhotoImage(file="Screens/assets/frame5/button_2.png")
-        button_2 = tk.Button(self,image=self.button_image_2,borderwidth=0,highlightthickness=0,command=lambda: controller.show_frame(el_tanıma_1),relief="flat")
-        button_2.place(x=15.0, y=113.0, width=215.0, height=85.0)
+        self.button_image_2 = tk.PhotoImage(file=relative_to_assets("button_2.png"))       
+        button_2 = tk.Button(self,image=self.button_image_2,borderwidth=0,highlightthickness=0,command=lambda: controller.show_frame(el_tanıma_1),relief="flat",)
+        button_2.place(x=15.0, y=116.0, width=450.0, height=85.0)
 
 
 
-        self.button_image_3 = tk.PhotoImage(file="Screens/assets/frame5/button_3.png")
-        button_3 = tk.Button(self,image=self.button_image_3,borderwidth=0,highlightthickness=0,command=lambda: controller.show_frame(optik),relief="flat")
-        button_3.place(x=249.0, y=113.0, width=215.0, height=85.0)
+        self.button_image_3 = tk.PhotoImage(file=relative_to_assets("button_3.png"))
+        button_3 = tk.Button(self,image=self.button_image_3,borderwidth=0,highlightthickness=0,command=self.speech_recognation_screen_controller,relief="flat")
+        button_3.place(x=15.0, y=222.0, width=450.0, height=85.0)
 
-        self.button_image_4 = tk.PhotoImage(file="Screens/assets/frame5/button_4.png")
-        button_4 = tk.Button(self,image=self.button_image_4,borderwidth=0,highlightthickness=0,command=self.speech_recognation_screen_controller ,relief="flat")
-        button_4.place(x=15.0, y=222.0, width=450.0, height=85.0)
-    
-        
+
+
     def speech_recognation_screen_controller(self):
         self.words = []
         self.controller.show_frame(speech_reco)
-
-
-
-        
-        
-
-
-
-
-
-
-
-class yazı(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        
-        
-
-
-
-        canvas = tk.Canvas(self,bg = "#47C4B6",height = 320,width = 480,bd = 0,highlightthickness = 0,relief = "ridge")
-        canvas.place(x = 0, y = 0)
-        self.button_image_1 =tk.PhotoImage(file="Screens/assets/frame0/button_1.png")
-        button_1 = tk.Button(self,image=self.button_image_1,borderwidth=0,highlightthickness=0,relief="flat") #,command=self.go_ahead
-        button_1.place(x=9.0, y=248.0, width=228.6839599609375, height=60.0)
-
-        button_2 = tk.Button(self,image=self.button_image_1,borderwidth=0,highlightthickness=0,relief="flat")#,command=self.go_ahead
-        button_2.place(x=242.0, y=248.0, width=228.6839599609375, height=60.0)
-
-
-        label_frame = tk.LabelFrame(self, background="#F0E2E7")
-        
-        label_frame.pack(expand=1, fill="both", side="bottom", pady=(0, 80)) 
-        self.controller = controller
-        
-
-        
-
-        
-
-        self.label_frame = tk.LabelFrame(self, background="#F0E2E7")
-        self.label_frame.pack(expand=1, fill="both", side="bottom", pady=(0, 80))
-        # self.camera = cv2.VideoCapture(0)
-        
-        # self.camera.set(4,1080)
-        # self.camera.set(3,1920)
-        # self.camera_label = tk.Label(self.label_frame)
-        # self.camera_label.pack()
-        # self.update_camera() 
-        
-        # Start updating camera stream
-    # def go_ahead(self):
-    #     self.camera.release()
-    #     self.controller.show_frame(el_tanıma_2)
-        
-        
-
-
-    # def update_camera(self):
-    #     # Function to continuously update the camera stream
-    #     ret, frame =self.camera.read()
-
-    #     if ret:
-    #         # Display the captured frame on the label
-            
-    #         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    #         image = cv2.rotate(image, cv2.ROTATE_180)
-    #         image= cv2.resize(image,(500,250))
-    #         image = Image.fromarray(image)
-            
-    #         image = ImageTk.PhotoImage(image=image)
-            
-    #         self.camera_label.configure(image=image)
-    #         self.camera_label.image = image  # type: ignore # Keep a reference to prevent garbage collection
-
-    #     # After 10 milliseconds, call the update_camera function again
-    #     self.after(10, self.update_camera)
-
-    
 
 
 
@@ -187,26 +108,29 @@ class optik(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
-        
+        #Frame 0
+        def relative_to_assets(file: str):
+            file= "Screens/assets/frame0/"+ file
+            return file
         
         
         canvas = tk.Canvas(self,bg = "#47C4B6",height = 320,width = 480,bd = 0,highlightthickness = 0,relief = "ridge")
 
         canvas.place(x = 0, y = 0)
-        self.button_image_1 = tk.PhotoImage(file="Screens/assets/frame1/button_1.png")
+        self.button_image_1 = tk.PhotoImage(file=relative_to_assets("button_1.png"))
         button_1 = tk.Button(self,image=self.button_image_1,borderwidth=0,highlightthickness=0,command=lambda: controller.show_frame(anasayfa),relief="flat")
-        button_1.place(x=152.0, y=246.0, width=160.0, height=70.0)
+        button_1.place(x=248.0, y=246.0, width=224.0, height=70.0)
 
 
 
 
-        self.button_image_2 = tk.PhotoImage(file="Screens/assets/frame1/button_2.png")
+
+        self.button_image_2 = tk.PhotoImage(file=relative_to_assets("button_2.png"))
         button_2 = tk.Button(self,image=self.button_image_2,borderwidth=0,highlightthickness=0,command=lambda: print("button_2 clicked"),relief="flat")
-        button_2.place(x=9.2552490234375, y=246.0, width=130.0, height=70.0)
+        button_2.place(x=11.0, y=246.0, width=224.0, height=70.0)
 
-        self.button_image_3 = tk.PhotoImage(file="Screens/assets/frame1/button_3.png")
-        button_3 = tk.Button(self,image=self.button_image_3,borderwidth=0,highlightthickness=0,command=lambda: print("button_3 clicked") ,relief="flat")
-        button_3.place(x=325.0, y=246.0, width=150.0, height=70.0)
+
+
 
 
         label_frame = tk.LabelFrame(self, background="#F0E2E7")
@@ -214,27 +138,47 @@ class optik(tk.Frame):
         label_frame.pack(expand=1, fill="both", side="bottom", pady=(0, 80)) 
 
 
-    
+
+
+
+
+
+
+
 class speech_reco(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         
+        # frame 3
+        def relative_to_assets(file: str):
+            file= "Screens/assets/frame3/"+ file
+            return file
+
 
         canvas = tk.Canvas(self, bg="#47C4B6", height=320, width=480, bd=0, highlightthickness=0, relief="ridge")
         canvas.place(x=0, y=0)
         
         self.controller = controller
-        self.button_image_1 = tk.PhotoImage(file="Screens/assets/frame4/button_1.png")
-        button_1 = tk.Button(self, image=self.button_image_1, borderwidth=0, highlightthickness=0, command=self.transcribe_and_update_label, relief="flat")  # ses kayıt butonu
+
+
+        self.button_image_1 = tk.PhotoImage(file=relative_to_assets("button_1.png"))
+        button_1 = tk.Button(self, image=self.button_image_1, borderwidth=0, highlightthickness=0, command=self.transcribe_and_update_label, relief="flat")  
         button_1.place(x=8.0, y=246.0, width=130.0, height=70.0)
 
-        self.button_image_2 = tk.PhotoImage(file="Screens/assets/frame4/button_2.png")
-        button_2 = tk.Button(self, image=self.button_image_2, borderwidth=0, highlightthickness=0, command=self.show_results, relief="flat")  # sonraki soru
+
+
+        self.button_image_2 = tk.PhotoImage(file=relative_to_assets("button_2.png"))
+        button_2 = tk.Button(self, image=self.button_image_2, borderwidth=0, highlightthickness=0, command=self.show_results, relief="flat")  
         button_2.place(x=147.0, y=246.0, width=142.8824462890625, height=70.0)
 
-        self.button_image_3 = tk.PhotoImage(file="Screens/assets/frame4/button_3.png")
-        button_3 = tk.Button(self, image=self.button_image_3, borderwidth=0, highlightthickness=0, command= self.navigate_and_show_results, relief="flat")  # bitir ve pdf al butonu
+
+
+        self.button_image_3 = tk.PhotoImage(file=relative_to_assets("button_3.png"))
+        button_3 = tk.Button(self, image=self.button_image_3, borderwidth=0, highlightthickness=0, command= self.navigate_and_show_results, relief="flat")  
         button_3.place(x=299.0, y=246.0, width=170.0, height=70.0)
+
+
+
 
         self.label_frame = tk.LabelFrame(self, background="#F0E2E7")
         self.label_frame.pack(expand=1, fill="both", side="bottom", pady=(0, 80))
@@ -288,7 +232,7 @@ class speech_reco(tk.Frame):
         transcription = self.transcribe_audio()
         
         # Update the label with the new transcription
-        self.transcription_label.config(text= transcription) # type: ignore
+        self.transcription_label.config(text= transcription)  # type: ignore
 
     def show_results(self):
         # Bu fonksiyon, "Sonraki Soru" butonuna tıklandığında çalışır.
@@ -309,9 +253,9 @@ class speech_reco(tk.Frame):
         if self.words:
             for kelime in self.words:
                 metin = metin + kelime + "\n"
-            
-            
-            output_path = "sesle_yazı.pdf"
+            desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')  # usb path eklenecek
+            filename = "sesle_yazı.pdf"
+            output_path = os.path.join(desktop_path, filename)
 
             write_to_pdf(output_path, metin)
             print(f"PDF dosyası oluşturuldu: ")
@@ -327,18 +271,23 @@ class el_tanıma_1(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        
+        #frame 1
+        def relative_to_assets(file: str):
+            file= "Screens/assets/frame1/"+ file
+            return file
 
         canvas = tk.Canvas(self, bg="#47C4B6", height=320, width=480, bd=0, highlightthickness=0, relief="ridge")
         canvas.place(x=0, y=0)
 
-        self.button_image_1 = tk.PhotoImage(file=   "Screens/assets/frame2/button_1.png")
-        button_1 = tk.Button(self, image=self.button_image_1, borderwidth=0, highlightthickness=0, relief="flat")#, command=self.take_photo  # şık foto çek
+        self.button_image_1 = tk.PhotoImage(file= relative_to_assets("button_1.png"))
+        button_1 = tk.Button(self, image=self.button_image_1, borderwidth=0, highlightthickness=0, relief="flat")#, command=self.take_photo  
         button_1.place(x=9.2552490234375, y=246.0, width=300.7447509765625, height=70.0)
 
-        self.button_image_2 = tk.PhotoImage(file="Screens/assets/frame2/button_2.png")
-        button_2 = tk.Button(self, image=self.button_image_2, borderwidth=0, highlightthickness=0, relief="flat")   #, command=self.go_ahead# devam
+
+        self.button_image_2 = tk.PhotoImage(file=relative_to_assets("button_2.png"))
+        button_2 = tk.Button(self, image=self.button_image_2, borderwidth=0, highlightthickness=0, relief="flat")   # command=self.go_ahead
         button_2.place(x=323.0, y=246.0, width=150.0, height=70.0)
+
 
         self.label_frame = tk.LabelFrame(self, background="#F0E2E7")
         self.label_frame.pack(expand=1, fill="both", side="bottom", pady=(0, 80))
@@ -416,15 +365,20 @@ class el_tanıma_2(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        #frame 2 
+        def relative_to_assets(file: str):
+            file= "Screens/assets/frame2/"+ file
+            return file
         
         
         self.controller = controller
         canvas = tk.Canvas(self,bg = "#47C4B6",height = 320,width = 480,bd = 0,highlightthickness = 0,relief = "ridge")
         
         canvas.place(x = 0, y = 0)
-        self.button_image_1 = tk.PhotoImage(file="Screens/assets/frame3/button_1.png")
-        button_1 = tk.Button(self,image=self.button_image_1,borderwidth=0,highlightthickness=0,command=lambda: controller.show_frame(anasayfa),relief="flat")# ses kaydı 
+        self.button_image_1 = tk.PhotoImage(file=relative_to_assets("button_1.png"))
+        button_1 = tk.Button(self,image=self.button_image_1,borderwidth=0,highlightthickness=0,command=lambda: controller.show_frame(anasayfa),relief="flat")
         button_1.place(x=44.0, y=245.0, width=391.035400390625, height=70.0)
+
 
         
         
